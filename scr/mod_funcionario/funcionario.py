d82b8e058fdca08df09cd5b16b7d00fe688cd262
arturfeldhauss <arturfeldhaus12@gmail.com>
2023-10-31 20:53:31 -0300

    att-13

--- scr/mod_funcionario/funcionario.py
+++ scr/mod_funcionario/funcionario.py
@@ -1,11 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import requests
+from settings import HEADERS_API, ENDPOINT_FUNCIONARIO
+
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
 
-''' rotas dos formulários '''
-@bp_funcionario.route('/', methods=['GET', 'POST'])
-def formListaFuncionario():
-    return render_template('formListaFuncionario.html')
+''' rotas anteriores omitidas '''
 
-@bp_funcionario.route('/form-funcionario/', methods=['GET'])
-def formFuncionario():
-    return render_template('formFuncionario.html')
+@bp_funcionario.route('/insert', methods=['POST'])
+def insert():
+try:
+        # dados enviados via FORM
+        id_funcionario = request.form['id']
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        grupo = request.form['grupo']
+        senha = request.form['senha']
+
+        # monta o JSON para envio a API
+        payload = {'id_funcionario': id_funcionario, 'nome': nome, 'matricula': matricula, 'cpf': cpf, 'telefone': telefone, 'grupo': grupo, 'senha': senha}
+
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(ENDPOINT_FUNCIONARIO, headers=HEADERS_API, json=payload)
+        result = response.json()
+
+        print(result)                       # [{'msg': 'Cadastrado com sucesso!', 'id': 13}, 200]
+        print(response.status_code)         # 200
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result[0])
+
+        return render_template('formListaFuncionario.html', msg=result[0])
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
--- scr/settings.py
+++ scr/settings.py
@@ -1,10 +1,18 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
-
+# localiza o arquivo de .env
+dotenv_file = find_dotenv()
 # Carrega o arquivo .env
-load_dotenv(".env")
-
+load_dotenv(dotenv_file)
 # Configurações da APP
 HOST = os.getenv("HOST")
 PORT = os.getenv("PORT")
 DEBUG = os.getenv("DEBUG")
+# Configurações da API
+URL_API = os.getenv("URL_API")
+HEADERS_API = {'x-token': os.getenv("X_TOKEN"), 'x-key': os.getenv("X_KEY")}
+# Configuração dos endpoints
+ENDPOINT_LOGIN = os.getenv("ENDPOINT_LOGIN")
+ENDPOINT_FUNCIONARIO = os.getenv("ENDPOINT_FUNCIONARIO")
+ENDPOINT_CLIENTE = os.getenv("ENDPOINT_CLIENTE")
+ENDPOINT_PRODUTO = os.getenv("ENDPOINT_PRODUTO")
