429f25abbf2c22565382c579d1f66cec3520ca7d
Felipe Kruchinschi Mortari <felipemkruch@email.com>
2023-12-05 19:27:46 -0300

    AppAval_3

--- scr/funcoes.py
+++ scr/funcoes.py
@@ -1,7 +1,10 @@
 from hashlib import sha3_256
+import logging
 
 class Funcoes(object):
-
-  @staticmethod
-  def cifraSenha(senha):
-    return sha3_256(senha.encode('utf-8')).hexdigest()
+    
+    @staticmethod
+    def cifraSenha(senha):
+        hashed_password = sha3_256(senha.encode('utf-8')).hexdigest()
+        logging.debug(f"Senha cifrada: {hashed_password}")
+        return hashed_password
